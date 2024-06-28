2e9c7df7d8dc105e49fe92b2270c841efe0b65cd
Mariia96 <mariasemen58@gmail.com>
2024-06-28 13:39:18 -0400

    Add second Item

--- cypress/e2e/cartAdd.spec.cy.js
+++ cypress/e2e/cartAdd.spec.cy.js
@@ -20,8 +20,43 @@ describe('Product Existence and Add to Cart on The Connected Shop', () => {
             .should('be.visible')
             .and('contain', 'Smart Door Lock')
 
-        // Optional: Verify that the product has been added to the cart
-        cy.get('.cart').should('contain', 'Smart Door Lock')
+        cy.get('.ProductMeta__Sku')
+            .should('be.visible')
+            .and('contain','200119')
+
+        // Optional: add to the cart
+        cy.get('.ProductForm__AddToCart')
+            .scrollIntoView()
+            .should('be.visible')
+            .click();
     });
 });
-    })
+    viewports.forEach(viewport => {
+    it('should find a product and add it to the cart 2 pieces', () => {
+       // Step 1: Visit the shop page of The Connected Shop
+        cy.viewport(viewport.width, viewport.height);
+        cy.visit('https://theconnectedshop.com/shop')
+
+        // Step 2: Ensure the product exists
+        cy.get('h2.ProductItem__Title')
+            .eq(1)
+            .scrollIntoView()
+            .should('be.visible')
+            .click();
+        cy.url()
+            .should('eq','https://theconnectedshop.com/products/smart-door-lock');
+        cy.get('.ProductMeta__Title')
+            .should('be.visible')
+            .and('contain', 'Smart Door Lock')
+        // Optional: Verify that the product has been added to the cart
+    cy.get('button[data-action="increase-quantity"]')
+        .should ("be.visible")
+        .click();
+        //Verify that quantity is 2
+        cy.get('.QuantitySelector__CurrentQuantity')
+            .should("be.visible")
+            .and('have.value', '2');
+    });
+    });
+});
+
